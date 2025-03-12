e0487f0028e08f166277aaca6ccade2c3b215214
Saakshi Shekhar <saakshi_shekhar@epam.com>
2025-03-12 10:00:32 +0530

    done task03

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,3 +1,11 @@
+// exports.handler = async (event) => {
+//     // TODO implement
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+//     return response;
+// };
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
